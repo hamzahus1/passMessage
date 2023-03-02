bfd5a113064418db7331a20133fdc33927e1df32
ittid <ahnaine3bd@gmail.com>
2023-03-02 15:31:55 +0100

    fix the trim validation

--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
-(function(){
-    document
+(function () {
+  document
     .getElementById("message-form")
     .addEventListener("submit", function (e) {
-        e.preventDefault();
-        const message = document.getElementById("message");
-        const value = message.value;
-       if(value === ''){
-           const feedback = document.querySelector('.feedback');
-           feedback.classList.add('show');
-           setTimeout(function(){
-               feedback.classList.remove('show')
-           },2000)
-       }
-        document.querySelector('.message-content').textContent = value;
-        message.value = '';
-
+      e.preventDefault();
+      const message = document.getElementById("message");
+      const value = message.value;
+      if (value === "") {
+        const feedback = document.querySelector(".feedback");
+        feedback.classList.add("show");
+        setTimeout(function () {
+          feedback.classList.remove("show");
+        }, 2000);
+      }
+      if (value.trim() === "") {
+        alert("please enter a valid text");
+        return;
+      }
+      document.querySelector(".message-content").textContent = value;
+      message.value = "";
     });
 })();
